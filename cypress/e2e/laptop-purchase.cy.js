af53692af5cc5b4d78ab1d706e9a77f6c4ce9250
anna_vdovenko <vdovenkoanna@gmail.com>
2026-05-18 22:57:36 +0200

    refactor: fixture alias pattern, per-test data generation, use selectors

--- cypress/e2e/laptop-purchase.cy.js
+++ cypress/e2e/laptop-purchase.cy.js
@@ -1,75 +1,68 @@
 import { faker } from "@faker-js/faker";
 import { clearCartIfNeeded } from "../helpers/api-helper";
+import { NAV, PRODUCT_PAGE, CART_PAGE, ORDER_MODAL } from "../support/selectors";
 
-const fullName = faker.person.fullName();
-const creditCard = faker.finance.creditCardNumber();
-let testData;
-
-function getLaptopPrice(priceString) {
+function getLaptopPrice(priceString, includeTaxString) {
   const priceStart = priceString.indexOf("$") + 1;
-  const priceEnd = priceString.indexOf(testData.includeTaxString) - 1;
+  const priceEnd = priceString.indexOf(includeTaxString) - 1;
   return priceString.slice(priceStart, priceEnd);
 }
 
 describe("Laptop purchase", () => {
-  beforeEach(() => {
-    cy.fixture("laptop-purchase-test-data").then((data) => {
-      testData = data;
-      cy.env(["username", "password"]).then(({ username, password }) => {
-        cy.login(username, password);
-      });
-      cy.visit("/");
-      cy.get("#nameofuser").should("be.visible");
+  beforeEach(function () {
+    cy.fixture("laptop-purchase-test-data").as("testData");
+    this.fullName = faker.person.fullName();
+    this.creditCard = faker.finance.creditCardNumber();
 
-      //adding cleanup in case previous test failed and left cart not empty
-      cy.then(() => clearCartIfNeeded());
-
-      // navigate to the list of Laptops
-      cy.get("#contcont").contains("Laptop").click();
+    cy.env(["username", "password"]).then(({ username, password }) => {
+      cy.login(username, password);
     });
+    cy.visit("/");
+    cy.get(NAV.USERNAME_DISPLAY).should("be.visible");
+    cy.then(() => clearCartIfNeeded());
+    cy.get(NAV.CATEGORIES).contains("Laptop").click();
   });
 
-  it(`User buys new Laptop`, () => {
-    //click on desirable model
+  it("User buys new Laptop", function () {
+    const { testData, fullName, creditCard } = this;
+
     cy.contains(testData.laptopModel).click();
-    // verify mandatory data on the page: Add to cart button, model name, price, description, image
-    cy.get("h2").invoke("text").should("have.string", testData.laptopModel);
+    cy.get(PRODUCT_PAGE.TITLE).invoke("text").should("have.string", testData.laptopModel);
+
     let priceString = "";
     let laptopPrice = "";
-    cy.get("h3")
+    cy.get(PRODUCT_PAGE.PRICE)
       .contains("$")
       .then(($text) => {
         priceString = $text.text();
       });
     cy.then(() => {
-      laptopPrice = getLaptopPrice(priceString);
-      cy.get("#myTabContent").then(($description) => {
+      laptopPrice = getLaptopPrice(priceString, testData.includeTaxString);
+
+      cy.get(PRODUCT_PAGE.DESCRIPTION).then(($description) => {
         expect($description.text()).contains(testData.descriptionSubtitle);
         expect($description.text().length).greaterThan(50);
       });
-      cy.get("#myCarousel-2").should("be.visible");
+      cy.get(PRODUCT_PAGE.IMAGE_CAROUSEL).should("be.visible");
 
-      //add itemToTheCart
       cy.clickElementAndVerifyAlert(
-        ".btn-success",
+        PRODUCT_PAGE.ADD_TO_CART_BTN,
         testData.alertTextOnAddToCart,
       );
 
-      // go to the cart to verify added product and total sum
-      cy.get("#cartur").click();
-      cy.get("#tbodyid .success").then((matchingElements) => {
+      cy.get(NAV.CART_BTN).click();
+      cy.get(CART_PAGE.ITEMS).then((matchingElements) => {
         expect(matchingElements.text())
           .contains(laptopPrice)
           .contains(testData.laptopModel);
         expect(matchingElements.length).eql(1);
       });
-      cy.get("#totalp").invoke("text").should("have.string", laptopPrice);
+      cy.get(CART_PAGE.TOTAL).invoke("text").should("have.string", laptopPrice);
 
-      // place order, fill in form with payment details
       cy.get("button").contains("Place Order").click();
-      cy.get("#orderModal").should("have.class", "show");
+      cy.get(ORDER_MODAL.MODAL).should("have.class", "show");
 
-      cy.get("#totalm").invoke("text").should("have.string", laptopPrice);
+      cy.get(ORDER_MODAL.TOTAL).invoke("text").should("have.string", laptopPrice);
       cy.fillInPlaceOrderModal({
         name: fullName,
         country: faker.location.country(),
@@ -82,68 +75,64 @@ describe("Laptop purchase", () => {
       cy.get("button").contains("Purchase").click();
       cy.verifySuccessPurchaseModal({
         title: testData.purchaseSuccessTitle,
-        fields: testData.orderConfirmationFields,
+        orderConfirmation: testData.orderConfirmation,
         name: fullName,
         card: creditCard,
         price: laptopPrice,
       });
       cy.get("button").contains("OK").click();
-      cy.get("#cat").should("be.visible");
+      cy.get(NAV.HOME_LOGO).should("be.visible");
     });
   });
 
-  it(`User selects laptop on second page, than changes his mind and remove it from the cart`, () => {
-    cy.get("#contcont").contains("Laptop").click();
-    cy.get("#tbodyid").should("be.visible");
-    cy.get("#next2").should("be.visible").click();
-    cy.get("#next2").should("be.hidden");
+  it("User selects laptop on second page, then changes his mind and removes it from the cart", function () {
+    const { testData } = this;
+
+    cy.get(CART_PAGE.TABLE_BODY).should("be.visible");
+    cy.get(CART_PAGE.NEXT_PAGE_BTN).should("be.visible").click();
+    cy.get(CART_PAGE.NEXT_PAGE_BTN).should("be.hidden");
     cy.contains(testData.cheapestLaptopModel).click();
-    cy.get("h2")
+    cy.get(PRODUCT_PAGE.TITLE)
       .invoke("text")
       .should("have.string", testData.cheapestLaptopModel);
-    //add itemToTheCart
+
     cy.clickElementAndVerifyAlert(
-      ".btn-success",
+      PRODUCT_PAGE.ADD_TO_CART_BTN,
       testData.alertTextOnAddToCart,
     );
-    cy.get("#cartur").click();
-    //verify that total has price
-    cy.get("#totalp")
+    cy.get(NAV.CART_BTN).click();
+    cy.get(CART_PAGE.TOTAL)
       .invoke("text")
       .should("have.string", testData.cheapestLaptopPrice);
-    cy.get("#tbodyid").contains("Delete").click();
-
-    //verify that total is empty
-    cy.get("#totalp").invoke("text").should("have.string", "");
+    cy.get(CART_PAGE.TABLE_BODY).contains("Delete").click();
+    cy.get(CART_PAGE.TOTAL).invoke("text").should("have.string", "");
   });
 
-  it(`Verify error if user submits Place Order modal empty, fill in required fields and verify success modal`, () => {
-    cy.fixture("laptop-purchase-test-data").then((data) => {
-      cy.get("#cartur").click();
-      cy.get("#totalp").invoke("text").should("have.string", "");
+  it("Verify error if user submits Place Order modal empty, fill in required fields and verify success modal", function () {
+    const { testData, fullName, creditCard } = this;
 
-      // place order, fill in form with payment details
-      cy.get("button").contains("Place Order").click();
-      cy.get("#orderModal").should("have.class", "show");
-      cy.clickElementAndVerifyAlert(
-        "#orderModal .btn-primary",
-        data["emptyOrderForm.ErrorMessage"],
-      );
+    cy.get(NAV.CART_BTN).click();
+    cy.get(CART_PAGE.TOTAL).invoke("text").should("have.string", "");
 
-      cy.fillInPlaceOrderModal({ name: fullName, card: creditCard });
+    cy.get("button").contains("Place Order").click();
+    cy.get(ORDER_MODAL.MODAL).should("have.class", "show");
+    cy.clickElementAndVerifyAlert(
+      ORDER_MODAL.SUBMIT_BTN,
+      testData["emptyOrderForm.ErrorMessage"],
+    );
 
-      cy.get("button").contains("Purchase").click();
+    cy.fillInPlaceOrderModal({ name: fullName, card: creditCard });
 
-      cy.verifySuccessPurchaseModal({
-        title: testData.purchaseSuccessTitle,
-        fields: testData.orderConfirmationFields,
-        name: fullName,
-        price: "0",
-        card: creditCard,
-      });
-      cy.get("button").contains("OK").click();
-      cy.get("#cat").should("be.visible");
+    cy.get("button").contains("Purchase").click();
+    cy.verifySuccessPurchaseModal({
+      title: testData.purchaseSuccessTitle,
+      orderConfirmation: testData.orderConfirmation,
+      name: fullName,
+      price: "0",
+      card: creditCard,
     });
+    cy.get("button").contains("OK").click();
+    cy.get(NAV.HOME_LOGO).should("be.visible");
   });
 
   afterEach(() => {
